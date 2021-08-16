c3468041bc2f2c6fa28eeca464c88c330e622089
24hourdeveloper <mikeitt009@gmail.com>
2021-08-16 01:28:16 -0400

    add production environment variables and added start script

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.PROD_USERNAME,
+    "password": process.env.PROD_PASSWORD,
+    "database": process.env.PROD_DATABASE,
+    "host": process.env.PROD_HOST,
+    "dialect": "postgres"
   }
 }
