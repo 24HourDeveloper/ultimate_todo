0747cc54c47bad107c88fa1614e1be725cf98043
24hourdeveloper <mikeitt009@gmail.com>
2021-08-16 00:52:01 -0400

    created environment variables, installed dotenv, changed config.json to config.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const {Todo} = require('./models')
 const app = express()
 const port = 3000
--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-{
-  "development": {
-    "username": "postgres",
-    "password": "password",
-    "database": "todo_database",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+module.exports = {
+  development: {
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    dialect: "postgres"
   },
   "test": {
     "username": "root",
