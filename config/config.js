bdfac30585607c18199efbccac562a5298b630ef
24hourdeveloper <mikeitt009@gmail.com>
2021-08-16 22:32:00 -0400

    removed strings from attributes

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
-    "use_env_variable": process.env.DATABASE_URL
+  production: {
+    use_env_variable: process.env.DATABASE_URL
   }
 }
